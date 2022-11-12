134330ef47095ef80c3c01e4a12450015e1362c8
Pranay Sood <pranay.sd@gmail.com>
2022-11-12 17:39:24 +0800

    creating and printing placeholders

--- Hangman.java
+++ Hangman.java
@@ -71,9 +71,21 @@ public class Hangman {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+
+        String word = randomWord();
+
+        // Need to replace the word with '_'
+        char[] placeholders = new char[word.length()];
+        for (int i = 0; i < placeholders.length; i++) {
+            placeholders[i] = '_';
+        }
+
         
 
 
+        
+        
+        
         scan.close();
 
 
@@ -86,6 +98,13 @@ public class Hangman {
         int numOfWords = words.length;
         int wordIndex = (int) (Math.random() * numOfWords);
         return words[wordIndex];
+    }
+
+    public static void printPlaceholders(char[] placeholders) {
+        for (int i = 0; i < placeholders.length; i++) {
+            System.out.print(placeholders[i] + " ");
+        }
+        System.out.print("\n");
     }
 
 }
